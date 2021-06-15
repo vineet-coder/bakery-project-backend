b85ce5a87c2ae7735b842155580b2a2d45193c2c
vineet-coder <vineetsinghal2312@gmail.com>
2021-06-16 01:53:31 +0530

    fix : heroku

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -19,7 +19,7 @@ const passwordAuthentication = (
           userEmail: email,
           userId: id,
         },
-        process.env.REACT_APP_SECRET_KEY,
+        "7vuz7pRy/hG8mTJlMXRcLnM38xGXFgNVuYAGYJTI1oqQFhSQFIHaRxu8pw453yJYP2juEit8pA8lDTvMNRtSQBM4RdzxOdpIX+cPrUCz9WSCnjqjIvCfXPGoqGJQl6REedrZUn95dySTu+G+vgtZK1ZHR8Cy7BcOCSg0jDpqBXVkx+wKMyHGtSlaLb1t+fqPmS+6MGW08CM4lqS9RdDcdAmAZ8lDL5WG/36Gas+04I0FXkchGv5UNcvbUfuFp/MzVsW12B8FJr6gRNNiPNEEEMSqBuhr8HU6kUHJvYiay6E3ah7pm5yVLIbIJ1XmfKsAP7GmBzg+2xwYayLJup2+Zw==",
         { expiresIn: "24h" }
       );
 
--- db/db.connection.js
+++ db/db.connection.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
-
 async function initializeDbConnection() {
   try {
     const response = await mongoose.connect(
-      process.env.REACT_APP_CONNECTION_STRING,
-
+      "mongodb+srv://vineet:cookiesbakery@cluster0.ymbhv.mongodb.net/shopstack?retryWrites=true&w=majority",
 
       {
         useFindAndModify: false,
